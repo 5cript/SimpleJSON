b5858b491a8ebaad4f3b65b7ff632f3c1e6f5576
5cript <Tim06TR@gmail.com>
2016-02-05 16:17:18 +0100

    Fixed 'unused variable options' warning

--- utility/base64.h
+++ utility/base64.h
@@ -118,7 +118,7 @@ namespace JSON
 		void resize (size_type n) { binary_.resize(n); }
 		void resize (size_type n, const value_type& val) { binary_.resize(n, val); }
 
-        std::ostream& stringify(std::ostream& stream, StringificationOptions const& options = DEFAULT_OPTIONS) const
+        std::ostream& stringify(std::ostream& stream, StringificationOptions const& = DEFAULT_OPTIONS) const
         {
             stream << "\"";
             encodeBase64(stream, binary_);
@@ -126,7 +126,7 @@ namespace JSON
             return stream;
         }
 
-        void parse(std::string const& name, PropertyTree const& tree, ParsingOptions const& options = DEFAULT_PARSER_OPTIONS)
+        void parse(std::string const& name, PropertyTree const& tree, ParsingOptions const& = DEFAULT_PARSER_OPTIONS)
         {
             std::string str = tree.tree.get<std::string>(name);
             decodeBase64(str, binary_);
