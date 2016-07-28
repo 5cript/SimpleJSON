b111ad2c43b8a3411da4287bc039f335c9dc4ce9
5cript <tim06tr@gmail.com>
2016-07-28 17:59:51 +0200

    Iterator traits are now enclosed in namespace.

--- stringify/iterator_traits.hpp
+++ stringify/iterator_traits.hpp
@@ -3,20 +3,23 @@
 #include <iterator>
 #include <type_traits>
 
-template <typename T>
-struct is_random_access_iterator : std::is_same <
-    typename std::iterator_traits<T>::iterator_category
-    , std::random_access_iterator_tag>
-{};
+namespace JSON
+{
+    template <typename T>
+    struct is_random_access_iterator : std::is_same <
+        typename std::iterator_traits<T>::iterator_category
+        , std::random_access_iterator_tag>
+    {};
 
-template <typename T>
-struct is_bidirectional_iterator : std::is_same <
-    typename std::iterator_traits<T>::iterator_category
-    , std::bidirectional_iterator_tag>
-{};
+    template <typename T>
+    struct is_bidirectional_iterator : std::is_same <
+        typename std::iterator_traits<T>::iterator_category
+        , std::bidirectional_iterator_tag>
+    {};
 
-template <typename T>
-struct is_forward_iterator : std::is_same <
-    typename std::iterator_traits<T>::iterator_category
-    , std::forward_iterator_tag>
-{};
+    template <typename T>
+    struct is_forward_iterator : std::is_same <
+        typename std::iterator_traits<T>::iterator_category
+        , std::forward_iterator_tag>
+    {};
+}
