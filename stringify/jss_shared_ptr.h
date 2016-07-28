4ea5e75d8eceed1ebfca6cafdb8d6f3d20f2517e
5cript <tim06tr@gmail.com>
2016-07-28 17:15:59 +0200

    Changed shared_ptr behaviour to new behaviour. Added shared_ptr to parser.

--- stringify/jss_shared_ptr.h
+++ stringify/jss_shared_ptr.h
@@ -2,17 +2,24 @@
 #define JSS_SHARED_PTR_H_INCLUDED
 
 #include "jss_core.h"
+#include "jss_smart_ptr.h"
+#include "../utility/polymorphy.h"
+
 #include <memory>
 
 namespace JSON
 {
     template <typename T,
-              typename = typename std::enable_if <Internal::can_stringify<T>::value>::type >
+              typename = typename std::enable_if <
+                Internal::can_stringify<T>::value ||
+                !std::is_same <typename polydecls <T>::type, no_poly>::value
+              >::type >
     std::ostream& stringify (std::ostream& stream, const std::string& name, std::shared_ptr<T> const& value, StringificationOptions const& options = DEFAULT_OPTIONS)
     {
         if (!value)
             throw SharedPtrNullptrException{};
-        return stringify(stream, name, *value, options);
+
+        return internal::stringify_smart_ptr(stream, name, value, options);
     }
 }
 
